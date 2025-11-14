0ab77221df5219ae97a0f6451509b9bb0615306b
Athish <Athish1123@gmail.com>
2025-11-14 13:08:22 -0600

    Made a fly enemy

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -1,23 +1,21 @@
 class_name Enemy
 extends CharacterBody2D
 
-@onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
-@export var movement_speed: float = 1.0
-var new_velocity: Vector2
-
 @onready var player = get_tree().get_nodes_in_group("players")[0]
+@onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
 
 
 
-func _process(delta: float) -> void:
-	moveEnemy()
-	
-func moveEnemy():
+#func _process(delta: float) -> void:
+	#move_enemy(1.0)
+
+func move_enemy(movement_speed: float):
+	print(player.global_position)
 	navigation_agent_2d.set_target_position(player.global_position)
 	velocity = global_position.direction_to(navigation_agent_2d.get_next_path_position()) * movement_speed
-	move_to_destination(velocity)
+	move_to_destination(velocity, movement_speed)
 	
-func move_to_destination(velocity : Vector2):
+func move_to_destination(velocity : Vector2, movement_speed: float):
 	global_position = global_position.move_toward(global_position + velocity, movement_speed)
 	
 	
