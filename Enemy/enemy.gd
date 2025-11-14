00894b6f4d717573bfc88bc446d589d1fd4424d2
Athish <Athish1123@gmail.com>
2025-11-13 18:38:02 -0600

    Enemy now moves towards player

---
+++ Enemy/enemy.gd
@@ -0,0 +1,24 @@
+class_name Enemy
+extends CharacterBody2D
+
+@onready var navigation_agent_2d: NavigationAgent2D = $NavigationAgent2D
+@export var movement_speed: float = 2.0
+var new_velocity: Vector2
+
+@onready var player = get_tree().get_nodes_in_group("players")[0]
+
+
+
+func _process(delta: float) -> void:
+	moveEnemy()
+	
+func moveEnemy():
+	print(player.global_position)
+	navigation_agent_2d.set_target_position(player.global_position)
+	velocity = global_position.direction_to(navigation_agent_2d.get_next_path_position()) * movement_speed
+	move_to_destination(velocity)
+	
+func move_to_destination(velocity : Vector2):
+	global_position = global_position.move_toward(global_position + velocity, movement_speed)
+	
+	
