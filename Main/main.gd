5737c1cbc2a6a962581df4cbf9ad3b395407578f
Athish <Athish1123@gmail.com>
2025-11-16 13:17:27 -0600

    Flixed merge

--- Main/main.gd
+++ Main/main.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var enemy = preload("res://Enemy/dash_enemy.tscn")
+var enemy = preload("res://Enemy/Fly.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,8 +10,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
 
 func _on_spawnTimer_timeout() -> void:
 	
