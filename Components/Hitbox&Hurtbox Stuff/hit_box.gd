a5cc0749e5a2c5e5cfb11be483d6d41919592dc3
Aria-Williams <159070680+Area9579@users.noreply.github.com>
2025-11-20 15:55:11 -0600

    On-hit particle effects

--- Components/Hitbox&Hurtbox Stuff/hit_box.gd
+++ Components/Hitbox&Hurtbox Stuff/hit_box.gd
@@ -2,7 +2,7 @@ class_name HitBox extends Area2D
 
 @export var health_component : HealthComponent
 @export var collision_shape2D : CollisionShape2D
-@export var hit_effect : PackedScene
+@export var hit_effect : ParticleEmitter
 @export var node_to_kill : Node
 
 func _ready() -> void:
@@ -16,7 +16,7 @@ func disable() -> void:
 
 func do_hit_effect() -> void:
 	if hit_effect != null:
-		add_child(hit_effect.instantiate())
+		hit_effect.emit(self)
 
 func _on_area_entered(area: Area2D) -> void:
 	
