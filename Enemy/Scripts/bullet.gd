29629a336954122a51ff9bc40cbbd0f2db892717
Aria-Williams <159070680+Area9579@users.noreply.github.com>
2025-11-22 20:24:01 -0600

    Bullets die after 2.5s

--- Enemy/Scripts/bullet.gd
+++ Enemy/Scripts/bullet.gd
@@ -1,8 +1,10 @@
 class_name Bullet extends CharacterBody2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 @onready var hurt_box: HurtBox = $HurtBox
 
 func _ready() -> void:
+	animation_player.play("bullet_fade")
 	hurt_box.doing_attack.connect(die)
 
 func _physics_process(_delta: float) -> void:
@@ -10,3 +12,7 @@ func _physics_process(_delta: float) -> void:
 
 func die():
 	self.queue_free()
+
+
+func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
+	die()
