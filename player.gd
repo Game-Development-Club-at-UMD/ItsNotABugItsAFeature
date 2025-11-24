5de36876fe8825d3e2fdd7ac321b47854583e492
Athish <Athish1123@gmail.com>
2025-11-23 20:11:25 -0600

    Player stops moving when attacking

--- player.gd
+++ player.gd
@@ -12,10 +12,14 @@ var target_position = Vector2()
 @onready var move_vis_rotation_helper: Node2D = $MoveVisRotationHelper
 @onready var movement_visualization: Node2D = $MoveVisRotationHelper/MovementVisualization
 
+
 @export var hitbox : HitBox
 @export var item : PackedScene
 
 var instanced_item : Item = null
+
+var state : States = States.PlAYER_MOVE
+enum States {PlAYER_MOVE, PLAYER_ATTACKING}
 
 func _ready() -> void:
 	hitbox.health_component.health = hitbox.health_component.max_health
@@ -26,21 +30,24 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	move_vis_rotation_helper.look_at(get_global_mouse_position())
-	
-	if Input.is_action_pressed("left_click"):
-		target_position = (get_global_mouse_position() - global_position).normalized()
-		if (get_global_mouse_position() - global_position).length() > 5:
-			velocity = lerp(velocity, target_position * SPEED, 12 * delta)
-		else:
+	match state:
+		States.PlAYER_MOVE:
+			if Input.is_action_pressed("left_click"):
+				target_position = (get_global_mouse_position() - global_position).normalized()
+				if (get_global_mouse_position() - global_position).length() > 5:
+					velocity = lerp(velocity, target_position * SPEED, 12 * delta)
+				else:
+					velocity = lerp(velocity, Vector2.ZERO, 12 * delta)
+			else:
+				velocity = lerp(velocity, Vector2.ZERO, 12 * delta)
+			if Input.is_action_just_pressed("right_click"):
+				state = States.PLAYER_ATTACKING
+				inventory.activate()
+			if Input.is_action_just_pressed("switch_item"):
+				inventory.swap_items()
+		States.PLAYER_ATTACKING:
 			velocity = lerp(velocity, Vector2.ZERO, 12 * delta)
-	else:
-		velocity = lerp(velocity, Vector2.ZERO, 12 * delta)
-	
-	if Input.is_action_just_pressed("right_click"):
-		inventory.activate()
-	
-	if Input.is_action_just_pressed("switch_item"):
-		inventory.swap_items()
+			waitForAttacking()
 	
 	move_and_slide()
 	update_anim_parameters()
@@ -51,7 +58,13 @@ func update_anim_parameters():
 
 func die():
 	player_died.emit()
+	
+func waitForAttacking():
+	await inventory.active_item.start_cooldown
+	state = States.PlAYER_MOVE
 
 func update_movement_visualization(dir : Vector2):
 	#print("instructed to update rotation")
 	movement_visualization.rotation = dir.angle()
+	
+	
