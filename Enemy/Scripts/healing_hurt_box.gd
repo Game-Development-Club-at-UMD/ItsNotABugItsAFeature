015bc45005025eebf518f535e7c538c383eddaa6
Aria-Williams <159070680+Area9579@users.noreply.github.com>
2025-11-23 22:31:44 -0600

    Added timer in healing orb

--- Enemy/Scripts/healing_hurt_box.gd
+++ Enemy/Scripts/healing_hurt_box.gd
@@ -2,11 +2,20 @@ class_name HealingHurtBox extends CharacterBody2D
 
 @onready var player : Player = get_tree().get_first_node_in_group("Player")
 
+@onready var timer: Timer = $Timer
+
+var following : bool = false
+
 func _physics_process(delta: float) -> void:
-	velocity = lerp(velocity, global_position.direction_to(player.global_position) * 200, 12 * delta)
+	if following:
+		velocity = lerp(velocity, global_position.direction_to(player.global_position) * 200, 12 * delta)
 	move_and_slide()
 
 
 func _on_hurt_box_body_entered(body: Node2D) -> void:
 	if body is Player:
 		queue_free()
+
+
+func _on_timer_timeout() -> void:
+	following = true
