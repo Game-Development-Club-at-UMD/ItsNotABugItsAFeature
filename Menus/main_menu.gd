c9b35173a018a4c7e2b10606157bfee2f8e22295
Eva Natalía <evaelvars@gmail.com>
2025-11-23 21:06:18 -0600

    added button text shake

--- Menus/main_menu.gd
+++ Menus/main_menu.gd
@@ -1,10 +1,13 @@
 extends Control
 
+func start_text_shake(node: Node, display_text: String):
+	node.text = ("[shake rate: 5.0 level -3 connected = 1][center]" + display_text + "[/center][/shake]")
 
+func stop_text_shake(node: Node, display_text: String):
+	node.text = "[center]" + display_text + "[/center]"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -16,3 +19,35 @@ func _on_play_pressed() -> void:
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+
+# Button shake shenanigans
+func _on_play_mouse_entered() -> void:
+	start_text_shake($VSplitContainer/MenuOptions/Play/PlayText, "Play")
+
+func _on_play_mouse_exited() -> void:
+	stop_text_shake($VSplitContainer/MenuOptions/Play/PlayText, "Play")
+
+func _on_settings_mouse_entered() -> void:
+	start_text_shake($VSplitContainer/MenuOptions/Settings/SettingsText, "Settings")
+
+
+func _on_settings_mouse_exited() -> void:
+	stop_text_shake($VSplitContainer/MenuOptions/Settings/SettingsText, "Settings")
+
+
+func _on_credits_mouse_entered() -> void:
+	start_text_shake($VSplitContainer/MenuOptions/Credits/CreditsText, "Credits")
+
+
+func _on_credits_mouse_exited() -> void:
+	stop_text_shake($VSplitContainer/MenuOptions/Credits/CreditsText, "Credits")
+
+
+func _on_quit_mouse_entered() -> void:
+	start_text_shake($VSplitContainer/MenuOptions/Quit/QuitText, "Quit")
+
+
+func _on_quit_mouse_exited() -> void:
+	stop_text_shake($VSplitContainer/MenuOptions/Quit/QuitText, "Quit")
